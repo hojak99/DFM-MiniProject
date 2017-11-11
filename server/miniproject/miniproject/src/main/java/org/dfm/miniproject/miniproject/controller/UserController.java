6aa19614fe1ca2882de2873959e3a5391590c950
KwonJH <hojak99@gmail.com>
2017-11-11 22:40:36 +0900

    글 작성 구현

--- server/miniproject/miniproject/src/main/java/org/dfm/miniproject/miniproject/controller/UserController.java
+++ server/miniproject/miniproject/src/main/java/org/dfm/miniproject/miniproject/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.dfm.miniproject.miniproject.dto.BoardDTO;
 import org.dfm.miniproject.miniproject.mapper.BoardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,7 +24,6 @@ public class UserController {
 	public String getUserBoardList() {
 		ModelAndView mav = new ModelAndView();
 		
-		
 		List<BoardDTO> boardList = boardMapper.getOpenBoardList();
 		System.out.println(boardList.toArray());
 		
@@ -31,6 +31,25 @@ public class UserController {
 		mav.addObject("type", "READ");
 		mav.addObject("list", boardList.toString());
 		
+		return mav.toString();
+	}
+	
+	@RequestMapping(value="/user/write.do")
+	public String writePost(@ModelAttribute BoardDTO boardDto) {
+		ModelAndView mav = new ModelAndView();
+		
+		try {
+			BoardDTO temp = new BoardDTO();
+			temp.setContent("insert~~~");
+			temp.setTitle("odw?");
+			temp.setUser_id("tootoo");
+			boardMapper.insertBoard(temp);
+		}catch (Exception e) {
+			e.printStackTrace();
+			return e.toString();
+		}
+		
+		mav.addObject("list", boardMapper.getOpenBoardList());
 		return mav.toString();
 	}
 }
--- server/miniproject/miniproject/src/main/java/org/dfm/miniproject/miniproject/mapper/BoardMapper.java
+++ server/miniproject/miniproject/src/main/java/org/dfm/miniproject/miniproject/mapper/BoardMapper.java
@@ -7,4 +7,5 @@ import org.dfm.miniproject.miniproject.dto.BoardDTO;
 public interface BoardMapper {
 	public List<BoardDTO> getAllBoardList();
 	public List<BoardDTO> getOpenBoardList();
+	public void insertBoard(BoardDTO boardDto);
 }
