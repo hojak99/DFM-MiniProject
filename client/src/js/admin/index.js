34f80963137a19ce4b418547ace11946dcdabab7
chs <18choi18@naver.com>
2017-11-11 16:56:07 +0900

    merge

--- client/src/js/admin/index.js
+++ client/src/js/admin/index.js
@@ -1,1 +1,29 @@
-console.log('hello admin');
+
+//처음 화면 로딩 데이터 랜더링
+$(document).ready(function(){
+  $.ajax({
+    type: 'get',
+    url: '',
+    dataType: "application/json",
+    success: function(data) {
+      if(data){
+        for(var item of data){
+          $("#list").append(
+            <li>
+              <p>item.title</p>
+              <div>item.content</div>
+              <p>
+                <span>item.crt_time</span> /
+                <span>item.nickname</span>
+              </p>
+            </li>
+          );
+        }
+        
+
+      }else{
+
+      }
+    }
+  })
+});
